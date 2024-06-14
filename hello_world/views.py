c80b15a65e672d014337ff04ccb68738da712fe3
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-06-14 20:49:17 +0200

    Creating a new App called About and add it to the settings.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse('<h1>This text should be shown as H1</h1>')
